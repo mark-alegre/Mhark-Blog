51ac44667690d97c0768879d05b0f8c71621db5a
mark-alegre <110073805+mark-alegre@users.noreply.github.com>
2023-11-30 18:25:56 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+import base64
 import streamlit as st
+import plotly.express as px
 from streamlit_option_menu import option_menu
 import home, account, your_post, about
 from PIL import Image
+
 
 img = Image.open('profile-pic.png')
 st.set_page_config(
@@ -17,46 +20,79 @@ hide_st_style = """
             """
 st.markdown(hide_st_style, unsafe_allow_html=True)
 
+df = px.data.iris()
 
+@st.cache_data
+def get_img_as_base64(file):
+    with open(file, "rb") as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+
+img = get_img_as_base64("tech2.jpg")
+
+page_bg_img = f"""
+<style>
+
+[data-testid="stSidebar"] > div:first-child {{
+background-image: url("data:image/png;base64,{img}");
+background-position: center; 
+background-repeat: no-repeat;
+background-attachment: fixed;
+}}
+
+[data-testid="stHeader"] {{
+background: rgba(0,0,0,0);
+}}
+
+[data-testid="stToolbar"] {{
+right: 2rem;
+}}
+</style>
+"""
+
+st.markdown(page_bg_img, unsafe_allow_html=True)
 
 class MultiApp:
 
-        def __init__(self):
-            self.apps = []
+    def __init__(self):
+        self.apps = []
 
-        def add_app(self, title, func):
+    def add_app(self, title, func):
 
-            self.apps.append({
-                "title": title,
-                "function": func
-            })
+        self.apps.append({
+            "title": title,
+            "function": func
+        })
 
-        def run():
-            # app = st.sidebar(
-            with st.sidebar:        
-                app = option_menu(
-                    menu_title='Mharks Blog',
-                    options=['Home','Recent Posts','Account','About'],
-                    icons=['house-fill','chat-fill','person-circle','info-circle-fill'],
-                    menu_icon='chat-text-fill',
-                    default_index=0,
-                    styles={
-                        "container": {"padding": "5!important","background-color":'black'},
-            "icon": {"color": "white", "font-size": "23px"}, 
-            "nav-link": {"color":"white","font-size": "20px", "text-align": "left", "margin":"0px", "--hover-color": "blue"},
-            "nav-link-selected": {"background-color": "#02ab21"},}
-                    
-                    )
+    def run():
+        # app = st.sidebar(
+        with st.sidebar:        
+            app = option_menu(
+                menu_title='Mharks Blog',
+                options=['Home','Recent Posts','Account','About'],
+                icons=['house-fill','chat-fill','person-circle','info-circle-fill'],
+                menu_icon='chat-text-fill',
+                default_index=0,
+                styles={
+                    "container": {"padding": "5!important","background-color":'black'},
+        "icon": {"color": "white", "font-size": "23px"}, 
+        "nav-link": {"color":"white","font-size": "20px", "text-align": "left", "margin":"0px", "--hover-color": "blue"},
+        "nav-link-selected": {"background-color": "#02ab21"},}
+                
+                )
 
-            
-            if app == "Home":
-                home.app()
-            if app == "Account":
-                account.app()          
-            if app == 'Recent Posts':
-                your_post.app()
-            if app == 'About':
-                about.app()    
-                       
-        run()            
-            
+        
+        if app == "Home":
+            home.app()
+        if app == "Account":
+            account.app()            
+        if app == 'Recent Posts':
+            your_post.app()
+        if app == 'About':
+            about.app()    
+             
+          
+             
+    run()            
+         
